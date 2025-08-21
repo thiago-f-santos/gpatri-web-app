1adfb8c6368ef6c42593b95af04067d493374bb1
Thiago <thiagoferreira2004f@gmail.com>
2025-08-21 13:46:30 -0300

    feat: mudando cor do botão ao estar invalido em formulario

--- src/app/shared/components/input/input.ts
+++ src/app/shared/components/input/input.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, forwardRef, HostBinding, Input } from '@angular/core';
-import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, forwardRef, HostBinding, Injector, Input, OnInit, Optional, Self } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './input.html',
   styleUrl: './input.scss',
@@ -15,19 +16,56 @@ import { NG_VALUE_ACCESSOR } from '@angular/forms';
     }
   ]
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor, OnInit{
+  private static nextId = 0;
+  public id = `app-input-${InputComponent.nextId++}`;
+
+  constructor(private injector: Injector) { }
+
   @Input() label!: string;
   @Input() type: 'text' | 'email' | 'password' | 'number' | 'textarea' = 'text';
   @Input() backgroundColor: string = '#03060F';
+  @Input() parentError: string | null = null;
   
   value: string = '';
   isDisabled: boolean = false;
+
+  public ngControl: NgControl | null = null;
+
+  ngOnInit(): void {
+    this.ngControl = this.injector.get(NgControl, null, { self: true, optional: true });
+    if (this.ngControl) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
 
   @HostBinding('class.textarea-host')
   get isTextarea(): boolean {
     return this.type === 'textarea';
   }
 
+  @HostBinding('class.has-error')
+  get hasErrorClass(): boolean {
+    return this.isInvalid;
+  }
+
+  public get isInvalid(): boolean {
+    if (!this.ngControl || !this.ngControl.control) {
+      return false;
+    }
+
+    const control = this.ngControl.control;
+    const form = control.parent;
+
+    const isControlInvalid = control.invalid && control.touched;
+
+    let hasParentError = false;
+    if (form && this.parentError) {
+      hasParentError = form.hasError(this.parentError) && control.touched;
+    }
+
+    return isControlInvalid || hasParentError;
+  }
   private onChange = (_: any) => {};
   private onTouched = () => {};
 
