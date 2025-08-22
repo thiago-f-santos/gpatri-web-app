3629ec415a32e203a9bf8accfc28d3a2eebe009f
Thiago <thiagoferreira2004f@gmail.com>
2025-08-22 18:48:06 -0300

    feat: fazendo conteudo do input ser mantido em atualização do componente caso necessário

--- src/app/shared/components/search-input/search-input.ts
+++ src/app/shared/components/search-input/search-input.ts
@@ -1,21 +1,55 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, forwardRef, Input, Output } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-search-input',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './search-input.html',
-  styleUrl: './search-input.scss'
+  styleUrl: './search-input.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SearchInput),
+      multi: true
+    }
+  ]
 })
-export class SearchInput {
+export class SearchInput implements ControlValueAccessor {
   @Input() placeholder: string = 'O que você está procurando?';
   @Output() search = new EventEmitter<string>;
 
-  searchQuery: string = '';
+  value: string = '';
+  isDisabled: boolean = false;
+
+  private onChange = (_: any) => {};
+  private onTouched = () => {};
+
+  writeValue(value: any): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onValueChange(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.value = target.value;
+    this.onChange(this.value);
+    this.onTouched();
+  }
 
   onSearch() {
-    this.search.emit(this.searchQuery);
+    this.search.emit(this.value);
   }
 }
